00529f5300839585a48dbb729fdedc3d1078b0cb
Mohamed Ashraf <medooka203@gmail.com>
2024-04-01 01:05:41 +0200

    Fixed script #7

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ q="Queue/Wx_Static_Queue.o"
 s="Stack/Wx_Static_Stack.o"
 # t="Tree/Wx_Static_Tree.o"
 # g="Graph/Wx_Static_Graph.o"
-cflags="-Os -g -std=c99"
+cflags="-g -std=c99"
 gcc main.c "$r$ll" "$r$q" "$r$s" -o output_test_1 "$cflags" -I"$plf"
 echo "======================================================"
 ls -la
