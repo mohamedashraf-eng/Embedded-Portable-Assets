81d96bd24d4dc41720bd842d238ce59a79d4e58f
Mohamed Ashraf <medooka203@gmail.com>
2024-04-01 00:58:29 +0200

    Fixed script #3

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 make clean
-make
 
 r="Core/PLibs/"
 plf="Core/Platform/"
 ll="LinkedList/Wx_Static_LinkedList.o"
 q="Queue/Wx_Static_Queue.o"
 s="Stack/Wx_Static_Stack.o"
-# t="Tree/Wx_Static_Queue.o"
-# g="Graph/Wx_Static_Queue.o"
+# t="Tree/Wx_Static_Tree.o"
+# g="Graph/Wx_Static_Graph.o"
 cflags="-O0 -g -std=c99"
 gcc main.c "$r$ll" "$r$q" "$r$s" -o output.exe "$cflags" -I"$plf"
 
 echo "======================================================"
-./output.exe
+sudo ./output.exe
 echo "======================================================"
