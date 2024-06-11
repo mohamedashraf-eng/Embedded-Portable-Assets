9371f349df4dcfd76cd28f2bd1cd30a294825cf5
Mohamed Ashraf <medooka203@gmail.com>
2024-06-11 13:09:10 +0000

    Added Reg types

--- software/Platform/Wx_SpecialTypes.h
+++ software/Platform/Wx_SpecialTypes.h
@@ -40,9 +40,72 @@ extern "C" {
  * @defgroup Special Data Types 
  * @{
  */
+/** 
+ * @brief 8-bit register structure with individual bit access.
+ */
+typedef union {
+    struct {
+        WX_U8 bit_0 : 1; /**< Bit 0 */
+        WX_U8 bit_1 : 1; /**< Bit 1 */
+        WX_U8 bit_2 : 1; /**< Bit 2 */
+        WX_U8 bit_3 : 1; /**< Bit 3 */
+        WX_U8 bit_4 : 1; /**< Bit 4 */
+        WX_U8 bit_5 : 1; /**< Bit 5 */
+        WX_U8 bit_6 : 1; /**< Bit 6 */
+        WX_U8 bit_7 : 1; /**< Bit 7 */
+    };
+    WX_U8 byte; /**< Full byte access */
+} WXReg8_t;
 
+/** 
+ * @brief 32-bit register structure with access to 8-bit segments and full 32-bit value.
+ */
+typedef union {
+    struct {
+        WXReg8_t byte_1; /**< Byte 1 */
+        WXReg8_t byte_2; /**< Byte 2 */
+        WXReg8_t byte_3; /**< Byte 3 */
+        WXReg8_t byte_4; /**< Byte 4 */
+    };
+    WX_U32 word; /**< Full 32-bit access */
+} WXReg32_t;
 
+/** 
+ * @brief 64-bit register structure with access to 8-bit segments, 32-bit segments and full 64-bit value.
+ */
+typedef union {
+    struct {
+        WXReg8_t byte_1; /**< Byte 1 */
+        WXReg8_t byte_2; /**< Byte 2 */
+        WXReg8_t byte_3; /**< Byte 3 */
+        WXReg8_t byte_4; /**< Byte 4 */
+        WXReg8_t byte_5; /**< Byte 5 */
+        WXReg8_t byte_6; /**< Byte 6 */
+        WXReg8_t byte_7; /**< Byte 7 */
+        WXReg8_t byte_8; /**< Byte 8 */
+    };
+    struct {
+        WXReg32_t word_1; /**< Word 1 */
+        WXReg32_t word_2; /**< Word 2 */
+    };
+    WX_U64 dword; /**< Full 64-bit access */
+} WXReg64_t;
 
+/**
+ * @brief 
+ * 
+ */
+typedef struct {
+
+} WXBigInt128_t;
+
+/**
+ * @brief 
+ * 
+ */
+typedef struct {
+
+} WXBigInt256_t;
 
 /** @} */
 
