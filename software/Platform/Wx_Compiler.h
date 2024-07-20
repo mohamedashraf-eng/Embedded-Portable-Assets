3da6da5aaff94d433db7cf00e1e015863baf275e
Mohamed Ashraf <medooka203@gmail.com>
2024-07-20 13:43:10 +0000

    Reogranized

--- software/Platform/Wx_Compiler.h
+++ software/Platform/Wx_Compiler.h
@@ -46,34 +46,33 @@ extern "C" {
  * @defgroup GCC_Compiler_Configurations GCC Compiler Configurations
  * @{
  */
-#    define _WX_NORETURN void
-#    define _WX_NOARG    void
+#    define _WX_NORETURN __attribute__((noreturn))
+#    define _WX_NOARG    __attribute__((noreturn))
 #    define _WX_STATIC   static
-#    define _WX_INLINE   inline
+#    define _WX_INLINE   inline __attribute__((always_inline))
 #    define _WX_CONST    const
-#    define _WX_LOCAL_INLINE _WX_STATIC _WX_INLINE
-#    define _WX_FORCE_CONST  __attribute__((_WX_CONST__))
-#    define _WX_PRINTF(a, b)  __attribute__((__format__(printf, a, b)))
-#    define _WX_SCANF(a, b)  __attribute__((__format__(scanf, a, b)))
-#    define _WX_MALLOC  __attribute__((__malloc__))
-#    define _WX_FORCE_NORETURN  _WX_NORETURN __attribute__((_WX_NORETURN__))
-#    define _WX_WEAK  __attribute__((__weak__))
-#    define _WX_COMPILETIME_WARNING(msg)  __attribute__((__warning__(msg)))
-#    define _WX_FORCE_INLINE _WX_INLINE __attribute__((__always_inline__))
-#    define _WX_FORCE_NOINLINE __attribute__((__noinline__))
-#    define _WX_FORCE_SIGNAL __attribute__((__signal__))
+#    define _WX_LOCAL_INLINE static inline
+#    define _WX_FORCE_CONST  __attribute__((const))
+#    define _WX_PRINTF(a, b)  __attribute__((format(printf, a, b)))
+#    define _WX_SCANF(a, b)  __attribute__((format(scanf, a, b)))
+#    define _WX_MALLOC  __attribute__((malloc))
+#    define _WX_WEAK  __attribute__((weak))
+#    define _WX_COMPILETIME_WARNING(msg)  __attribute__((warning(msg)))
+#    define _WX_FORCE_INLINE __attribute__((always_inline))
+#    define _WX_FORCE_NOINLINE __attribute__((noinline))
+#    define _WX_FORCE_SIGNAL __attribute__((signal))
 #    define _WX_FORCE_SECTION(__SECTION) __attribute__((section(__SECTION)))
 #    define _WX_USED __attribute__((used))
 #    define _WX_ALIGN(n) __attribute__((aligned(n)))
 #    define _WX_PACKED_STRUCT __attribute__((packed))
-#    define _WX_COMPILETIME_ERROR(msg)  __attribute__((__error__(msg)))
-#    define _WX_FORCE_UNUSED  __attribute__((__unused__))
-#    define _WX_MUST_CHECK   __attribute__((__warn_unused_result__))
+#    define _WX_COMPILETIME_ERROR(msg)  __attribute__((error(msg)))
+#    define _WX_FORCE_UNUSED  __attribute__((unused))
+#    define _WX_MUST_CHECK   __attribute__((warn_unused_result))
 #    define _WX_OPTIMIZE_O0 __attribute__((optimize("O0"))) /**< No optimization */
 #    define _WX_OPTIMIZE_O1 __attribute__((optimize("O1"))) /**< Optimize for size */
 #    define _WX_OPTIMIZE_O2 __attribute__((optimize("O2"))) /**< Optimize for speed */
 #    define _WX_OPTIMIZE_O3 __attribute__((optimize("O3"))) /**< Aggressive optimization */
-#    define _WX_OPTIMIZE_OG __attribute__((optimize("OG"))) /**< Optimize for debugging */
+#    define _WX_OPTIMIZE_OG __attribute__((optimize("Og"))) /**< Optimize for debugging */
 #    define _WX_LIKELY(cond) __builtin_expect(!!(cond), 1)
 #    define _WX_UNLIKELY(cond) __builtin_expect(!!(cond), 0)
 #    define _WX_THREAD_LOCAL __thread
@@ -88,12 +87,12 @@ extern "C" {
  * @defgroup Clang_Compiler_Configurations Clang Compiler Configurations
  * @{
  */
-#    define _WX_NORETURN void
-#    define _WX_NOARG    void
+#    define _WX_NORETURN __attribute__((noreturn))
+#    define _WX_NOARG    __attribute__((noreturn))
 #    define _WX_STATIC static
-#    define _WX_INLINE inline
+#    define _WX_INLINE inline __attribute__((always_inline))
 #    define _WX_CONST  const
-#    define _WX_LOCAL_INLINE _WX_STATIC _WX_INLINE
+#    define _WX_LOCAL_INLINE static inline
 #    define _WX_FORCE_CONST __attribute__((const))
 #    define _WX_FORCE_INLINE __attribute__((always_inline))
 #    define _WX_FORCE_NOINLINE __attribute__((noinline))
@@ -108,7 +107,7 @@ extern "C" {
 #    define _WX_OPTIMIZE_O1 __attribute__((optimize("O1"))) /**< Optimize for size */
 #    define _WX_OPTIMIZE_O2 __attribute__((optimize("O2"))) /**< Optimize for speed */
 #    define _WX_OPTIMIZE_O3 __attribute__((optimize("O3"))) /**< Aggressive optimization */
-#    define _WX_OPTIMIZE_OG __attribute__((optimize("OG"))) /**< Optimize for debugging */
+#    define _WX_OPTIMIZE_OG __attribute__((optimize("Og"))) /**< Optimize for debugging */
 #    define _WX_THREAD_LOCAL _Thread_local
 #    define _WX_NULL (void *)0
 #    define INTERRUPT_FUNC
@@ -119,7 +118,6 @@ extern "C" {
 #endif /* COMPILER_USED */
 
 /** @} */ /* End of CompilerSpecific */
-
 /*----------------------------------------------------------------------------*/
 #if defined(__cplusplus) || defined(c_plusplus)
 }
